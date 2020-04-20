3e148ac934ccc03982357b1c2b724e4d3e605383
tupaginaonline <contacto@tupaginaonline.net>
2020-04-20 09:38:50 -0430

    second upload

--- src/database.ts
+++ src/database.ts
@@ -10,7 +10,8 @@ export async function getConnection():Promise<mysql.Connection> {
 		host:process.env.HOST_CLEVER_CLOUD,
 		user:process.env.USER_CLEVER_CLOUD,
 		password:process.env.PASSWORD_CLEVER_CLOUD,
-		database:process.env.DATABASE_CLEVER_CLOUD
+        database:process.env.DATABASE_CLEVER_CLOUD,
+		connectionLimit:10
 		
 	});
 
