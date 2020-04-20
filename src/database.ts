928a1c89f5eb0ff65551e7929e851bea6a737c2d
tupaginaonline <contacto@tupaginaonline.net>
2020-04-19 19:46:50 -0430

    change main.hbs

--- src/database.ts
+++ src/database.ts
@@ -7,10 +7,9 @@ export async function getConnection():Promise<mysql.Connection> {
 	
 	const conex = await mysql.createConnection({
 		
-		host:process.env.HOST_CLEVER_CLOUD,
-		user:process.env.USER_CLEVER_CLOUD,
-		password:process.env.PASSWORD_CLEVER_CLOUD,
-		database:process.env.DATABASE_CLEVER_CLOUD
+		host:process.env.HOST_LOCAL,
+		user:process.env.USER_LOCAL,
+		database:process.env.DATABASE_LOCAL
 		
 	});
 
